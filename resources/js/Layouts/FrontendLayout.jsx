5683c551e94c9588efdb11a0cff37e390e4f6d4a
Rockrayhan <khayrulalamdict@gmail.com>
2024-03-27 12:40:21 +0600

    first commit

---
+++ resources/js/Layouts/FrontendLayout.jsx
@@ -0,0 +1,25 @@
+import Footer from '@/Pages/Shared/Footer';
+import Header from '@/Pages/Shared/Header';
+import { usePage } from '@inertiajs/react';
+import React from 'react';
+
+
+const FrontendLayout = ({ children }) => {
+    const {  userData , products, categories, reviews, students, loginMsg} = usePage().props; 
+    //used usepage().props rather than direct distructuring (like children) because it can't get data 
+    const { user, token } = userData ;
+
+    return (
+        <div>
+<Header userData={userData} ></Header>
+
+        <main>
+    {children}
+        </main>
+
+    <Footer></Footer>
+        </div>
+    );
+};
+
+export default FrontendLayout;
